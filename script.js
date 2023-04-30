abdee7deb525d7758ee0285e2face7f7d7ce287a
kevoaac <130163890+kevoaac@users.noreply.github.com>
2023-04-30 15:28:58 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// //java
+// public static void main (String[]){
+
+//     System.out.printl("Hola mundo")
+// }
+// //array
+// Array arr = new Array(Integer);
+// arr= [2,3,4];
+
+// int resultado = Math.pow(2,6);
+// //c++
+// #inpud <Input <stdio .h>
+
+// main (
+//     {
+//         printf ("hola mundod");
+//         getc;
+//     }
+// )
+// JS
+// console.log('Hola mundo');
+
+// let result = Math.pow(2,6)
+// console.log(result);
+
+
+
+let miArray = [2, 3,5 ,6, 7,102]
+
+console.log(miArray)
